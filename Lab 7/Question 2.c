d64a992ecf6dc51549cb5f41eecbc186df738b38
FatimaSalmann <fatima.salman430@gmail.com>
2024-10-10 14:25:57 +0500

    Question 2.c

---
+++ Lab 7/Question 2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+	
+    char input[20];
+    int a = 0, e = 0, i = 0, o = 0, u = 0;
+    int f;
+
+    // scanf user input
+    printf("Enter a string: ");
+    scanf("%s", input); 
+
+    // Checking the vowels
+    for (f = 0; input[f] != '\0'; f++) {
+        if (input[f] == 'a' || input[f] == 'A') a++;
+        else if (input[f] == 'e' || input[f] == 'E')
+		e++;
+        else if (input[f] == 'i' || input[f] == 'I') 
+		i++;
+        else if (input[f] == 'o' || input[f] == 'O') 
+		o++;
+        else if (input[f] == 'u' || input[f] == 'U') 
+		u++;
+    }
+
+    // Printing the results
+    printf("a=%d, e=%d, i=%d, o=%d, u=%d\n", a, e, i, o, u);
+    return 0;
+}
